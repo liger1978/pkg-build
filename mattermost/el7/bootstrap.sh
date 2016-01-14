acd9250adaf74c5a596b11716813f09ad8ce4cd1
grainger@gmail.com <grainger@gmail.com>
2016-01-14 22:08:01 +0000

    Working on mattermost packaging

--- mattermost/el7/bootstrap.sh
+++ mattermost/el7/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+moddir='/tmp/vagrant-puppet/environments/vagrant/modules'
 which gem >/dev/null 2>&1 || {
   echo Installing rubygems...
   yum -y install rubygems
@@ -16,25 +17,3 @@ which rpmbuild >/dev/null 2>&1 || {
   yum -y install rpm-build
 }
 
-which cpanm >/dev/null 2>&1 || {
-  echo Installing perl-App-cpanminus...
-  yum -y install perl-App-cpanminus
-}
-
-[ -e /usr/share/perl5/vendor_perl/local/lib.pm ] || {
-  echo Installing perl-local-lib...
-  yum -y install perl-local-lib
-}
-
-which sqlplus64 >/dev/null 2>&1 || {
-  echo Installing Oracle Instant Client...
-  wget http://mir-cern.ihep.ac.cn/cern/centos/7/cernonly/x86_64/Packages/oracle-instantclient11.2-basic-11.2.0.4.0-1.x86_64.rpm
-  wget http://mir-cern.ihep.ac.cn/cern/centos/7/cernonly/x86_64/Packages/oracle-instantclient11.2-devel-11.2.0.4.0-1.x86_64.rpm
-  wget http://mir-cern.ihep.ac.cn/cern/centos/7/cernonly/x86_64/Packages/oracle-instantclient11.2-sqlplus-11.2.0.4.0-1.x86_64.rpm
-  yum -y localinstall ./oracle-instantclient11.2-basic-11.2.0.4.0-1.x86_64.rpm
-  yum -y localinstall ./oracle-instantclient11.2-devel-11.2.0.4.0-1.x86_64.rpm
-  yum -y localinstall ./oracle-instantclient11.2-sqlplus-11.2.0.4.0-1.x86_64.rpm
-}
-
-cd /vagrant
-make
